c465f5679a90e91ed649d307d15d6192ec6fbac0
hermanpouokam <85396309+hermanpouokam@users.noreply.github.com>
2025-02-25 20:06:58 +0100

    fix api url

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,80 +1,11 @@
-// import { NextResponse } from "next/server";
-
-// export async function authMiddleware(req) {
-//     const token = req.cookies.get("access_token")?.value;
-//     const requestedUrl = req.nextUrl.clone();
-//     const pathname = requestedUrl.pathname;
-
-//     // ✅ Permettre l'accès à /signin et /signup SANS token
-//     if (!token) {
-//         if (pathname === "/signin" || pathname === "/signup") {
-//             console.log("🔓 Accès autorisé à /signin ou /signup sans token");
-//             return NextResponse.next();
-//         }
-//         console.log("❌ Pas de token, redirection vers /signin");
-//         const signInUrl = new URL("/signin", req.url);
-//         signInUrl.searchParams.set("next", requestedUrl.pathname);
-//         return NextResponse.redirect(signInUrl);
-//     }
-
-//     // 🔄 Rediriger un utilisateur connecté depuis /signin et /signup
-//     if (token) {
-//         try {
-//             const response = await fetch("http://127.0.0.1:8000/api/verify-token/", {
-//                 method: "POST",
-//                 headers: {
-//                     Authorization: `Bearer ${token}`,
-//                     "Content-Type": "application/json",
-//                 },
-//                 body: JSON.stringify({ token }),
-//             });
-
-//             if (response.ok) {
-//                 console.log("✅ Token valide");
-//                 if (pathname === ("/signin") || pathname === ("/signup")) {
-//                     console.log("🔄 Utilisateur connecté, redirection vers /dashboard");
-//                     return NextResponse.redirect(new URL("/dashboard", req.url));
-//                 }
-
-//                 return NextResponse.next();
-//             } else {
-//                 // 🔄 Tentative de rafraîchissement du token
-//                 if (refresh_token) {
-//                     const newToken = await refreshToken(refresh_token);
-//                     if (newToken) {
-//                         console.log("🔄 Token rafraîchi");
-//                         const nextResponse = NextResponse.next();
-//                         nextResponse.cookies.set("access_token", newToken, {
-//                             maxAge: LONG_LIFE_DURATION,
-//                             path: "/",
-//                         });
-//                         return nextResponse;
-//                     }
-//                 }
-//                 console.log("❌ Token invalide, redirection vers /signin");
-//                 const signInUrl = new URL("/signin", req.url);
-//                 signInUrl.searchParams.set("next", requestedUrl.pathname);
-//                 return NextResponse.redirect(signInUrl);
-//             }
-//         } catch (err) {
-//             console.error("❌ Erreur lors de la vérification du token:", err.message);
-//             const signInUrl = new URL("/signin", req.url);
-//             signInUrl.searchParams.set("next", requestedUrl.pathname);
-//             return NextResponse.redirect(signInUrl);
-//         }
-//     }
-
-//     // ✅ Si le token existe et la route est protégée => Continue
-//     return NextResponse.next();
-// }
-
 import { NextResponse } from "next/server";
 import { LONG_LIFE_DURATION } from "@/utils/constants";
+import API_URL from "@/config";
 
 // 🔄 Fonction pour rafraîchir le token
 async function refreshToken(refreshToken) {
     try {
-        const response = await fetch("http://127.0.0.1:8000/api/token/refresh/", {
+        const response = await fetch(`${API_URL}/token/refresh/`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
@@ -115,7 +46,7 @@ export async function authMiddleware(req) {
 
     // ✅ Vérification du token
     try {
-        const response = await fetch("http://127.0.0.1:8000/api/verify-token/", {
+        const response = await fetch(`${API_URL}/verify-token/`, {
             method: "POST",
             headers: {
                 Authorization: `Bearer ${token}`,
