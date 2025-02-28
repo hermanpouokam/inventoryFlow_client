bb8f8c1053e7b944a6aaa1658c73841eb4b03be9
hermanpouokam <85396309+hermanpouokam@users.noreply.github.com>
2025-02-28 10:34:20 +0100

    creating landing page

---
+++ src/components/phoneVersion.tsx
@@ -0,0 +1,96 @@
+"use client";
+
+import React, { useState } from "react";
+import { Dialog, DialogPanel } from "@headlessui/react";
+import { MenuIcon, X } from "lucide-react";
+
+export const links = [
+  {
+    title: "Nos services",
+    link: "#services",
+  },
+  {
+    title: "Temoignages",
+    link: "#testimony",
+  },
+  {
+    title: "Pricing",
+    link: "#pricing",
+  },
+  {
+    title: "A propos de nous",
+    link: "#about-us",
+  },
+  {
+    title: "Nous contacter",
+    link: "#contact",
+  },
+];
+
+export default function PhoneVersion() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  return (
+    <div>
+      <div className="flex lg:hidden">
+        <button
+          type="button"
+          onClick={() => setMobileMenuOpen(true)}
+          className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+        >
+          <span className="sr-only">Open main menu</span>
+          <MenuIcon aria-hidden="true" className="size-6" />
+        </button>
+      </div>
+      <Dialog
+        open={mobileMenuOpen}
+        onClose={setMobileMenuOpen}
+        className="lg:hidden"
+      >
+        <div className="fixed inset-0 z-50" />
+        <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div className="flex items-center justify-between">
+            <a href="#" className="-m-1.5 p-1.5">
+              <span className="sr-only">Your Company</span>
+              <img
+                alt=""
+                src="https://tailwindui.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
+                className="h-8 w-auto"
+              />
+            </a>
+            <button
+              type="button"
+              onClick={() => setMobileMenuOpen(false)}
+              className="-m-2.5 rounded-md p-2.5 text-gray-700"
+            >
+              <span className="sr-only">Close menu</span>
+              <X aria-hidden="true" className="size-6" />
+            </button>
+          </div>
+          <div className="mt-6 flow-root">
+            <div className="-my-6 divide-y divide-gray-500/10">
+              <div className="space-y-2 py-6">
+                {links.map((item) => (
+                  <a
+                    key={item.title}
+                    href={item.link}
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                  >
+                    {item.title}
+                  </a>
+                ))}
+              </div>
+              <div className="py-6">
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                >
+                  Log in
+                </a>
+              </div>
+            </div>
+          </div>
+        </DialogPanel>
+      </Dialog>
+    </div>
+  );
+}
